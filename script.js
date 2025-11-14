e7ce7f7812a2f4f5ded0aa37bac76fcbc16d73f8
ohm-pkh <Phudol.kh@gmail.com>
2025-11-14 21:38:54 +0700

    Implement: Profile

--- script.js
+++ script.js
@@ -8,4 +8,19 @@ const observer = new IntersectionObserver((entries) => {
   });
 });
 
-document.querySelectorAll("#Home").forEach(el => observer.observe(el));
+document.querySelectorAll("#Home, #Profile").forEach(el => observer.observe(el));
+
+
+const container = document.querySelector('.infoContainer');
+const dots = document.querySelectorAll('.dot');
+
+dots.forEach(dot => {
+    dot.addEventListener('click', () => {
+        const index = dot.getAttribute('data-index');
+        container.scrollLeft = container.clientWidth * index;
+
+        // Update active class
+        dots.forEach(d => d.classList.remove('active'));
+        dot.classList.add('active');
+    });
+});
