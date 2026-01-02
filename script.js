21e0106592ef3d7fb14ec73167e31c51dc48f0cc
ohm-pkh <phudol.kh@gmail.com>
2026-01-02 11:46:26 +0700

    Project section

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 class Project {
-  constructor(name, description, state) {
+  constructor(name,img, description, state) {
     const stateEnum = ["Planing","Hold", "Design", "Dev", "Test", "Demo", "Ended"];
 
     if (!stateEnum.includes(state)) {
@@ -10,6 +10,7 @@ class Project {
 
     this.name = name;
     this.description = description;
+    this.img = img || null;
     this.state = state;
   }
 }
@@ -27,6 +28,15 @@ class ProjectArr {
     this.projects.push(project);
   }
 
+  findNextBlank(txt,init){
+    if (txt.length <= init) return txt;
+    let i = init
+    while (txt[init] !== ' '){
+      i += 1
+    }
+    return txt.slice(0,init)
+  }
+
   createContainer(id){
     const container = document.getElementById(id);
     let HTMLCode =""
@@ -34,8 +44,12 @@ class ProjectArr {
       HTMLCode += `
         <div class="projectCard">
           <h3>${p.name}</h3>
-          <p>${p.description}</p>
-          <span class="state ${p.state}">${p.state}</span>
+          <div class=projectImage ${p.img?`style = "background-image: url('${p.img}')"`:""}>${p.img?'':'No Image'}</div>
+          <p>${p.description.length<=750?p.description:this.findNextBlank(p.description,745) + '...'}</p>
+          <div class="stateContainer">
+            <span class="state ${p.state}">${p.state}</span>
+          </div>
+          
         </div>
       `;
     });
@@ -72,7 +86,7 @@ dots.forEach(dot => {
 });
 
 
-const MovieTicketBookingSystem = new Project("Movie ticket booking system",
+const MovieTicketBookingSystem = new Project("Movie ticket booking system",null,
                                                 `Developed a web-based movie ticket 
                                                 booking system that include booking part and
                                                 system management part by
@@ -83,7 +97,7 @@ const MovieTicketBookingSystem = new Project("Movie ticket booking system",
                                                 including UX/UI and database design.`,
                                                 `Ended`
                                               );
-const FraudDetectionProject = new Project("Fraud detection project",
+const FraudDetectionProject = new Project("Fraud detection project",null,
                                           `This project was created to address the problem of 
                                           fraudulent transactions. My team and I developed data 
                                           visualizations to explore fraud patterns and built a 
@@ -95,17 +109,17 @@ const FraudDetectionProject = new Project("Fraud detection project",
                                           from Kaggle to support our analysis and model development.`,
                                           `Ended`
                                         );
-const WanNeeGinaraiDee = new Project("Wan nee gin arai dee system",
+const WanNeeGinaraiDee = new Project("Wan nee gin arai dee system",null,
                                       `From the Daily question like "What should we/I eat?"
                                       that almost burn our break time or sometime it can be the cause
                                       that make conflict in relationship. This project are create for
                                       help people answer this question easier to save break time
                                       , stay relationship and help people make decision easier by provide
                                       some of decision making tools and unique feature`,
-                                      `Demo`
+                                      `Ended`
                                     );
 
-const BuyingNote = new Project("Buying Note",
+const BuyingNote = new Project("Buying Note",null,
                                       `This project is created from the problem 
                                       that I often forget to buy some items that 
                                       were already listed in the chat. And when 
@@ -116,7 +130,7 @@ const BuyingNote = new Project("Buying Note",
                                       order status, and share them with others.`,
                                       `Hold`
                                     );
-const MymeiProject = new Project("E-commerce management system",
+const MymeiProject = new Project("E-commerce management system",null,
                                       `Developed a web-based e-commerce management 
                                       system using HTML, CSS, and JavaScript for the 
                                       frontend, and PHP for the backend, with MySQL 
