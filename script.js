fc305ad7e02ea4ed9b1947a4bb47b1872dfe7923
ohm-pkh <phudol.kh@gmail.com>
2026-01-05 14:00:57 +0700

    inti link open add dv project link

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 class Project {
-  constructor(name,img, description, state) {
-    const stateEnum = ["Planing","Hold", "Design", "Dev", "Test", "Demo", "Ended"];
+  constructor(name, img, description, state, link) {
+    const stateEnum = ["Planing", "Hold", "Design", "Dev", "Test", "Demo", "Ended"];
 
     if (!stateEnum.includes(state)) {
       throw new Error(
@@ -12,6 +12,7 @@ class Project {
     this.description = description;
     this.img = img || null;
     this.state = state;
+    this.link = link || null;
   }
 }
 
@@ -28,34 +29,49 @@ class ProjectArr {
     this.projects.push(project);
   }
 
-  findNextBlank(txt,init){
+  findNextBlank(txt, init) {
     if (txt.length <= init) return txt;
     let i = init
-    while (txt[i] !== ' ' && i < txt.length){
+    while (txt[i] !== ' ' && i < txt.length) {
       i += 1
     }
-    return txt.slice(0,i)
+    return txt.slice(0, i)
   }
 
-  createContainer(id){
+  createContainer(id) {
     const container = document.getElementById(id);
-    let HTMLCode =""
-    this.projects.reverse().forEach(p => {
-      HTMLCode += `
-        <div class="projectCard">
-          <h3>${p.name}</h3>
-          <div class=projectImage ${p.img?`style = "background-image: url('${p.img}')"`:""}>${p.img?'':'No Image'}</div>
-          <p>${p.description.length<=750?p.description:this.findNextBlank(p.description,745) + '...'}</p>
-          <div class="stateContainer">
-            <span class="state ${p.state}">${p.state}</span>
-          </div>
-          
-        </div>
-      `;
-    });
 
-    container.innerHTML = HTMLCode;
+    this.projects.slice().reverse().forEach(p => {
+      const obj = document.createElement('div');
+      obj.classList.add('projectCard');
+
+      const HTMLCode = `
+      <h3>${p.name}</h3>
+      <div class="projectImage"
+        ${p.img ? `style="background-image: url('${p.img}')"` : ""}>
+        ${p.img ? "" : "No Image"}
+      </div>
+      <p>
+        ${p.description.length <= 750
+          ? p.description
+          : this.findNextBlank(p.description, 745) + '...'}
+      </p>
+      <div class="stateContainer">
+        <span class="state ${p.state}">${p.state}</span>
+      </div>
+    `;
+
+      obj.innerHTML = HTMLCode;
+
+      if (p.link) {
+        obj.style.cursor = 'pointer';
+        obj.onclick = () => window.open(p.link, '_blank');
+      }
+
+      container.appendChild(obj);
+    });
   }
+
 }
 
 const observer = new IntersectionObserver((entries) => {
@@ -86,8 +102,8 @@ dots.forEach(dot => {
 });
 
 
-const MovieTicketBookingSystem = new Project("Movie ticket booking system",null,
-                                                `Developed a web-based movie ticket 
+const MovieTicketBookingSystem = new Project("Movie ticket booking system", null,
+  `Developed a web-based movie ticket 
                                                 booking system that include booking part and
                                                 system management part by
                                                 using HTML, CSS, and
@@ -95,10 +111,10 @@ const MovieTicketBookingSystem = new Project("Movie ticket booking system",null,
                                                 with Express.js for the backend, with PostgreSQL 
                                                 for the database. Performed full-stack development,
                                                 including UX/UI and database design.`,
-                                                `Ended`
-                                              );
-const FraudDetectionProject = new Project("Fraud detection project",null,
-                                          `This project was created to address the problem of 
+  `Ended`, `https://drive.google.com/drive/folders/15NbVBP_vAA3VWONvvYxmTIXx3gTeiY80?usp=sharing`
+);
+const FraudDetectionProject = new Project("Fraud detection project", null,
+  `This project was created to address the problem of 
                                           fraudulent transactions. My team and I developed data 
                                           visualizations to explore fraud patterns and built a 
                                           classification model to detect fraudulent transactions.
@@ -107,20 +123,20 @@ const FraudDetectionProject = new Project("Fraud detection project",null,
                                           data analysis (EDA). We used SAR data from 
                                           FinCEN.gov along with open-source datasets 
                                           from Kaggle to support our analysis and model development.`,
-                                          `Ended`
-                                        );
-const WanNeeGinaraiDee = new Project("Wan nee gin arai dee system",null,
-                                      `From the Daily question like "What should we/I eat?"
+  `Ended`
+);
+const WanNeeGinaraiDee = new Project("Wan nee gin arai dee system", null,
+  `From the Daily question like "What should we/I eat?"
                                       that almost burn our break time or sometime it can be the cause
                                       that make conflict in relationship. This project are create for
                                       help people answer this question easier to save break time
                                       , stay relationship and help people make decision easier by provide
                                       some of decision making tools and unique feature`,
-                                      `Ended`
-                                    );
+  `Ended`
+);
 
-const BuyingNote = new Project("Buying Note",null,
-                                      `This project is created from the problem 
+const BuyingNote = new Project("Buying Note", null,
+  `This project is created from the problem 
                                       that I often forget to buy some items that 
                                       were already listed in the chat. And when 
                                       people assign buying notes in a group, it 
@@ -128,15 +144,15 @@ const BuyingNote = new Project("Buying Note",null,
                                       project is made to solve that by giving the 
                                       ability to create buying orders, track the 
                                       order status, and share them with others.`,
-                                      `Hold`
-                                    );
-const MymeiProject = new Project("E-commerce management system",null,
-                                      `Developed a web-based e-commerce management 
+  `Hold`
+);
+const MymeiProject = new Project("E-commerce management system", null,
+  `Developed a web-based e-commerce management 
                                       system using HTML, CSS, and JavaScript for the 
                                       frontend, and PHP for the backend, with MySQL 
                                       for the database. Performed full-stack development.`,
-                                      `Ended`
-                                    );
+  `Ended`
+);
 const projectContainer = new ProjectArr(MymeiProject);
 projectContainer.append(MovieTicketBookingSystem);
 projectContainer.append(FraudDetectionProject);
